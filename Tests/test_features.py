ce49164fea2e25e3acebb7200f5928ee70a76808
nulano <nulano@nulano.eu>
2019-09-22 16:34:58 +0200

    test_features update

--- Tests/test_features.py
+++ Tests/test_features.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 import io
+import os
 
 from PIL import features
 
@@ -16,12 +17,16 @@ except ImportError:
 
 class TestFeatures(PillowTestCase):
     def test_check(self):
+        # Check every check_xxx function returns a boolean
         # Check the correctness of the convenience function
         for module in features.modules:
+            self.assertIn(features.check_module(module), [True, False])
             self.assertEqual(features.check_module(module), features.check(module))
         for codec in features.codecs:
+            self.assertIn(features.check_codec(codec), [True, False])
             self.assertEqual(features.check_codec(codec), features.check(codec))
         for feature in features.features:
+            self.assertIn(features.check_feature(feature), [True, False])
             self.assertEqual(features.check_feature(feature), features.check(feature))
 
     @unittest.skipUnless(HAVE_WEBP, "WebP not available")
@@ -39,29 +44,21 @@ class TestFeatures(PillowTestCase):
     def test_webp_anim(self):
         self.assertEqual(features.check("webp_anim"), _webp.HAVE_WEBPANIM)
 
-    def test_check_modules(self):
-        for feature in features.modules:
-            self.assertIn(features.check_module(feature), [True, False])
-        for feature in features.codecs:
-            self.assertIn(features.check_codec(feature), [True, False])
-
-    def test_supported_modules(self):
+    def test_supported(self):
         self.assertIsInstance(features.get_supported_modules(), list)
         self.assertIsInstance(features.get_supported_codecs(), list)
         self.assertIsInstance(features.get_supported_features(), list)
         self.assertIsInstance(features.get_supported(), list)
 
-    def test_unsupported_codec(self):
-        # Arrange
-        codec = "unsupported_codec"
-        # Act / Assert
-        self.assertRaises(ValueError, features.check_codec, codec)
+    @unittest.skipUnless(hasattr(os.environ, "CI_FEATURES"), "CI_FEATURES not set")
+    def test_supported_ci(self):
+        self.assertEqual(features.get_supported().sort(), os.environ["CI_FEATURES"].split(",").sort())
 
-    def test_unsupported_module(self):
-        # Arrange
-        module = "unsupported_module"
-        # Act / Assert
-        self.assertRaises(ValueError, features.check_module, module)
+    def test_check_unsupported(self):
+        self.assertRaises(ValueError, features.check_codec, "unsupported")
+        self.assertRaises(ValueError, features.check_module, "unsupported")
+        self.assertRaises(ValueError, features.check_feature, "unsupported")
+        self.assertRaises(ValueError, features.check, "unsupported")
 
     def test_pilinfo(self):
         buf = io.StringIO()
